c1e8640d5cbc9f7e2f4779c0a15540d06de9fde2
Bruno Costa <bruno.costa@strix.pt>
2021-04-23 21:12:26 +0100

    Adiciona documentos do projeto 2

---
+++ trabalho2/IsyIESD/CesVector/SerVector/SerVectorOPE/src/main/java/isos/tutorial/isyiesd/cesvector/servector/Vector.java
@@ -0,0 +1,23 @@
+package isos.tutorial.isyiesd.cesvector.servector;
+
+import javax.jws.WebService;
+import java.util.Arrays;
+import java.util.List;
+
+@WebService(endpointInterface = "isos.tutorial.isyiesd.cesvector.servector.IVector")
+public class Vector implements IVector {
+
+    private static List<Integer> vector = Arrays.asList(300, 234, 56, 789);
+
+    @Override
+    public int read(int pos) {
+        System.out.println("Reading from vector position " + pos);
+        return vector.get(pos);
+    }
+
+    @Override
+    public void write(int pos, int n) {
+        System.out.println("Writing to vector in position " + pos + " with " + n);
+        vector.set(pos, n);
+    }
+}
